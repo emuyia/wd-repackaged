5ec70aad9c37d5968b25598d6a93399380bbf40a
em <emuyiahere@gmail.com>
2023-10-01 04:49:38 +0100

    implem oj zt start/install

--- wdlaunch/OJZT.cs
+++ wdlaunch/OJZT.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.VisualBasic;
+using System.Drawing;
 
 namespace WDLaunch
 {
@@ -98,6 +100,67 @@ namespace WDLaunch
 			Process process = new Process { StartInfo = startInfo };
 			process.Start();
 			process.WaitForExit();
+		}
+
+		public static async Task InstallOrStartZeroTier()
+		{
+			// Define the expected path of ZeroTier executable
+			string programData = Environment.GetEnvironmentVariable("ProgramData");
+			string executablePath = Path.Combine(programData, @"ZeroTier\One\zerotier-one_x64.exe");
+
+			// Check if the ZeroTier executable exists
+			if (File.Exists(executablePath))
+			{
+				// Start ZeroTier
+				string programFiles32 = Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+				string ZTdesktop = Path.Combine(programFiles32, @"ZeroTier\One\zerotier_desktop_ui.exe");
+				Process.Start(ZTdesktop);
+			}
+			else
+			{
+				// Define the URL of the ZeroTier installer
+				string installerUrl = "https://download.zerotier.com/dist/ZeroTier%20One.msi";
+
+				// Define the path to save the ZeroTier installer
+				string installerPath = Path.Combine(Path.GetTempPath(), "ZeroTierOne.msi");
+
+				// Create a new WebClient instance
+				using (WebClient client = new WebClient())
+				{
+					// Create a new Form with a ProgressBar and a Label
+					Form form = new Form { Size = new Size(400, 100),
+										   ShowIcon = false,
+										   StartPosition = FormStartPosition.CenterScreen,
+										   ControlBox = false,
+										   SizeGripStyle = SizeGripStyle.Hide };
+
+					ProgressBar progressBar = new ProgressBar { Dock = DockStyle.Bottom, Maximum = 100 };
+					Label label = new Label { Dock = DockStyle.Fill, TextAlign = ContentAlignment.MiddleCenter };
+					form.Controls.Add(label);
+					form.Controls.Add(progressBar);
+
+					// Hook up a handler to the WebClient.DownloadProgressChanged event
+					client.DownloadProgressChanged += (sender, e) =>
+					{
+						// Update the ProgressBar and Label on the UI thread
+						progressBar.Value = e.ProgressPercentage;
+						label.Text = $"Download progress: {e.ProgressPercentage}%";
+					};
+
+					// Show the Form
+					form.Show();
+
+					// Start the download task
+					await client.DownloadFileTaskAsync(new Uri(installerUrl), installerPath);
+
+					// Close the Form
+					form.Close();
+				}
+
+				// Start the ZeroTier installer
+				Process installerProcess = Process.Start(installerPath);
+				installerProcess.WaitForExit();
+			}
 		}
 	}
 }
